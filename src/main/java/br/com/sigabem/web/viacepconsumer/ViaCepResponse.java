eefd2254db2e3bb0d1dba39dc3b399092133dd09
Tiago Pereira <tiagopereiracx@gmail.com>
2022-02-06 16:55:38 -0300

    Classe para mapear cada resposta da api ViaCep

---
+++ src/main/java/br/com/sigabem/web/viacepconsumer/ViaCepResponse.java
@@ -0,0 +1,12 @@
+package br.com.sigabem.web.viacepconsumer;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ViaCepResponse {
+    private String ddd;
+    private String uf;
+}
