3f0f2c8c4f7873574b39a6227434b9729288d829
Tiago Pereira <tiagopereiracx@gmail.com>
2022-02-08 00:14:58 -0300

    Classe para tramento de exceções retornadas pela api ViaCep

---
+++ src/main/java/br/com/sigabem/exception/ViaCepConsumerException.java
@@ -0,0 +1,7 @@
+package br.com.sigabem.exception;
+
+public class ViaCepConsumerException extends RuntimeException{
+    public ViaCepConsumerException(String message) {
+        super(message);
+    }
+}
