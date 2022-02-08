9c14736fd2db4c040da421fd7ec1b5f2b1a2e061
Tiago Pereira <tiagopereiracx@gmail.com>
2022-02-07 21:46:40 -0300

    Inserção de configuração geral da documentação

--- src/main/java/br/com/sigabem/SigaBemApplication.java
+++ src/main/java/br/com/sigabem/SigaBemApplication.java
@@ -1,9 +1,23 @@
 package br.com.sigabem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+		@Info(title = "Siga Bem - API",
+				description = "Api para gerar cotação (valor e tempo de entrega) " +
+						"a partir de cep de origem e cep de destino.",
+				version = "1.0",
+				contact = @Contact(
+						name = "Tiago Pereira da Silva",
+						email = "tiagopereiracx@gmail.com",
+						url = "https://github.com/tiagopereiradasilva"
+				)
+		))
 public class SigaBemApplication {
 
 	public static void main(String[] args) {
