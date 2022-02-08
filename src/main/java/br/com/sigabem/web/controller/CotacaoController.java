389f51cb9e396848129ada1c6bb1248b581aad00
Tiago Pereira <tiagopereiracx@gmail.com>
2022-02-07 21:47:37 -0300

    Inserção de detalhamento na documentação para o endpoint cotacao

--- src/main/java/br/com/sigabem/web/controller/CotacaoController.java
+++ src/main/java/br/com/sigabem/web/controller/CotacaoController.java
@@ -3,6 +3,11 @@ package br.com.sigabem.web.controller;
 import br.com.sigabem.service.CotacaoService;
 import br.com.sigabem.web.dto.CotacaoRequestDTO;
 import br.com.sigabem.web.dto.CotacaoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,14 @@ public class CotacaoController {
         this.cotacaoService = cotacaoService;
     }
 
+    @Operation(summary = "Criar cotação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Sucesso ao criar cotação",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CotacaoResponseDTO.class))}
+            )
+    })
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CotacaoResponseDTO> salvar(@RequestBody CotacaoRequestDTO cotacaoRequestDTO){
