fb66ef0c02f9ff9e5da3f3ff05fe806a74ce6ada
Tiago Pereira <tiagopereiracx@gmail.com>
2022-02-08 00:17:28 -0300

    Adição de anotações para validação dos atributos

--- src/main/java/br/com/sigabem/web/dto/CotacaoRequestDTO.java
+++ src/main/java/br/com/sigabem/web/dto/CotacaoRequestDTO.java
@@ -2,12 +2,22 @@ package br.com.sigabem.web.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 
 @Getter
 @Setter
 public class CotacaoRequestDTO {
+    @NotNull(message = "Campo peso é obrigatório")
     private float peso;
+    @NotBlank(message = "Campo cepOrigem é obrigatório")
     private String cepOrigem;
+    @NotBlank(message = "Campo cepDestino é obrigatório")
     private String cepDestino;
+    @NotBlank(message = "Campo nomeDestinatario é obrigatório")
     private String nomeDestinatario;
 }
